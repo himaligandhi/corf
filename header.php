5358eea4d805a4ac57a67fe1879be519cd41793b
himali gandhi <himali.gandhi@triosstudent.com>
2020-05-19 19:45:43 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,66 @@
+<!--Navigation Start-->
+<nav class="navbar navbar-expand-sm navbar-dark" style="margin-bottom: 15px;">
+   <div class="container-fluid">
+   <a class="navbar-brand js-scroll-trigger" href="about.php">
+                <img class="img-fluid" src="images/logo2.png" alt="" style="height:80px; margin-left:25px;margin-top: 30px;"/>
+            </a>
+            <!-- <h3 style="color:black;margin-left:25px;">FINANCIAL RESTRUCTURING SPECIALISTS</h3> -->
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="navbarsExampleCenteredNav" aria-expanded="false" aria-label="Toggle navigation" style="border-color:#8B040B;background-color: #8B040B;">
+      <span class="navbar-toggler-icon"></span>
+      </button>    
+      <div class="collapse navbar-collapse navbar-collapse justify-content-md-end" id="mainNav">
+        
+      <a href="cred.php" class="sim-btn hvr-bounce-to-top" style="font-size: 1.5em;margin-right:30px;"> FINANCE APPLICATION: APPLY NOW</a>
+      <ul class="navbar-nav social-icons">         
+                            <li><a class="facebook" href="https://www.facebook.com/CORFinancial-Corp-118192885553051/"><i class="fa fa-facebook facebook-bg"></i></a></li>
+                            <li><a class="instagram" href="https://www.instagram.com/cor_financial/"><i class="fa fa-instagram"></i></a></li>
+                            <!--<li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>-->
+                            <li><a class="linkedin" href="https://www.linkedin.com/company/corfinancialcorp/"><i class="fa fa-linkedin"></i></a></li>
+            </ul>    
+      </div>
+   </div>
+</nav>
+<nav class="navbar navbar-expand-sm navbar-dark" style="margin-bottom: 0px;">
+   <div class="container-fluid" style="border-top:0.5px solid #44444C; border-bottom: 0.5px solid #44444C;background-color: #ececed;">
+     
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="navbarsExampleCenteredNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>    
+      <div class="collapse navbar-collapse navbar-collapse justify-content-md-center" id="mainNav">
+         <ul class="navbar-nav">
+         <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="index.php">ABOUT US</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="LendingSolutions.php">LENDING SOLUTIONS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="LendingStructure.php">LENDING STRUCTURE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="Industry.php">INDUSTRIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="CORsolutions.php">CORSOLUTIONS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="transcation.php">TRANSACTIONS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="blog.php">INSIGHTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="Partnerships.php">PARTNERSHIPS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="Contact.php">CONTACT US</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="CreditApplication.php">Finance Application</a>
+                    </li> -->
+                
+                
+         </ul>
+      </div>
+   </div>
+</nav>
